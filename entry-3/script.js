a1f65cde991044f338ac2ccd02147c26c9be1276
angelinaao <aoangelina302@gmail.com>
2024-12-03 20:53:00 -0500

    Add files via upload

---
+++ entry-3/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $(".pflower-img").hover(function(){
+        $(".room-img").toggle()
+        $(".blue-ruby").toggle()
+        $(".smallruby-img").toggle()
+        $(".tank").toggle()
+
+
+    })
+
+
+
+})
